18f95b72899a807fc26d467dbd476a12031fae72
Vitalii Savchuk <esvit666@gmail.com>
2013-11-02 12:52:11 +0200

    add example

--- bz-dragndrop.src.js
+++ bz-dragndrop.src.js
@@ -37,31 +37,75 @@ app.directive('bzDraggable', ['$rootScope', function ($rootScope) {
         }
     }
 }]);
-app.directive('bzDraggable', ['$rootScope', function ($rootScope) {
+app.directive('bzDroppable', ['$rootScope', '$parse', function ($rootScope, $parse) {
     return {
-        restrict: 'A',
-        require: 'bzData',
-        link: function (scope, element, attr, controller) {
-            attr.$set('draggable', true);
+        scope: true,
+        link: function (scope, element, attr) {
+            attr.$set('droppable', true);
+
+            if (attr.bzDroppable) {
+                scope.validate = $parse(attr.bzDroppable);
+            }
+            if (attr.drop) {
+                scope.drop = $parse(attr.drop);
+            }
 
             $rootScope.$$dragElements = $rootScope.$$dragElements || [];
 
-            element.bind('dragstart', function (e) {
-                e.dataTransfer.effectAllowed = 'move';
-
-                $rootScope.$$dragElements[controller.$$hashKey] = controller;
-                $rootScope.$$currentData = controller;
-                e.dataTransfer.setData('text', controller.$$hashKey);
-                element.addClass('drag');
-                $rootScope.$emit('bzDragStart', element);
+            var isValid = function(data) {
+                if (!angular.isFunction(scope.validate)) {
+                    return true;
+                }
+                return scope.validate(scope, { '$data': data });
+            };
+            var getData = function(e) {
+                var id = e.dataTransfer.getData('text'),
+                    controller = $rootScope.$$dragElements[id] || $rootScope.$$currentData;
+                if (angular.isUndefined(controller)) {
+                    return undefined; // temp fix https://bugs.webkit.org/show_bug.cgi?id=23695
+                }
+                return controller.data();
+            };
+            element.bind('dragover', function(e) {
+                e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+                if (e.preventDefault) {
+                    e.preventDefault(); // Necessary. Allows us to drop.
+                }
+                if (!isValid(getData(e))) {
+                    return true;
+                }
+                element.addClass('over');
                 return false;
             });
+            element.bind('dragenter', function(e) {
+                if (isValid(getData(e))) {
+                    element.addClass('over');
+                }
+            });
 
-            element.bind('dragend', function(e) {
-                element.removeClass('drag');
-                $rootScope.$$dragElements.splice($rootScope.$$dragElements.indexOf(controller.$$hashKey), 1);
-                $rootScope.$$currentData = undefined;
-                $rootScope.$emit('bzDragEnd', element);
+            element.bind('dragleave', function(e) {
+                if (isValid(getData(e))) {
+                    element.removeClass('over');
+                }
+            });
+
+            element.bind('drop', function(e) {
+                if (!isValid(getData(e))) {
+                    return true;
+                }
+                element.removeClass('over');
+                if (e.preventDefault) {
+                    e.preventDefault(); // Necessary. Allows us to drop.
+                }
+
+                if (e.stopPropogation) {
+                    e.stopPropogation(); // Necessary. Allows us to drop.
+                }
+                // call the passed drop function
+                scope.$apply(function (scope) {
+                    scope.drop(scope, { '$data': getData(e) });
+                });
+                return false;
             });
         }
     }
